0357ae85b9f79e7a45ac26d021c31973162b8617
Swapneel <vswapneel27@gmail.com>
2024-02-01 13:23:02 -0500

    Bug number 7

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,6 @@ export function App() {
   )
 
   const loadAllTransactions = useCallback(async () => {
-    transactionsByEmployeeUtils.invalidateData()
 
     if(employees == null){
       setIsLoading(true)
